21d96311fe3fe37239698a43f1f9c5d127e92aff
zSkipe <valquea22@gmail.com>
2024-01-19 20:43:25 -0700

    changes

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,10 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
+
+        Console.WriteLine("What is your favorite color? ");
+        string  color = Console.ReadLine();
+        Console.WriteLine(color);
+        Console.WriteLine($"Your color is {color} ");
     }
 }
